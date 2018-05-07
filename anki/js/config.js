0c7b2b1e44cda158dd70dedaa730c7cd77233d34
Stuart Coggins <develop@coggs.net.au>
2018-05-07 10:51:15 +1000

    Anki Push

---
+++ anki/js/config.js
@@ -0,0 +1,5 @@
+var apex_url = '/apex/PDB1/f?p=22790:25';
+var dbmon_url = 'http://analytics.opcau.com:9704/va/project.jsp';
+var docs_url = 'http://anki.opcau.com/IoTRaceTrack/';
+var cloud_url = 'https://www.youtube.com/watch?v=QD5v7Fgh3Tk';
+var cards = { apex: true, dbmon: true, docs: true, cloud: true };
