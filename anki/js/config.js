1f1eacc389c65a94a1d67f797a5e4067ece9ecd9
Stuart Coggins <develop@coggs.net.au>
2018-08-03 07:57:02 +0800

    ADW update
    Update with ADW message and new URLS

--- anki/js/config.js
+++ anki/js/config.js
@@ -1,5 +1,5 @@
-var apex_url = 'http://anki.opcau.com/apex/PDB1/f?p=22790:25';
-var dbmon_url = 'http://analytics.opcau.com:9704/va/project.jsp';
-var docs_url = 'http://opcau.github.io/anki//IoTRaceTrack/';
+var apex_url = 'https://anki.opcau.com/apex/PDB1/f?p=22790:25';
+var dbmon_url = 'https://analytics.opcau.com/dv/ui';
+var docs_url = 'https://opcau.github.io/anki//IoTRaceTrack/';
 var cloud_url = 'https://www.youtube.com/watch?v=QD5v7Fgh3Tk';
 var cards = { apex: true, dbmon: true, docs: true, cloud: true };
